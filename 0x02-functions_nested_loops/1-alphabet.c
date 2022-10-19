e4e8f4cf9294512aa42129d244222bc6abc7ba48
Trust Gumise <trust@tolvatechafrica.com>
2022-10-18 17:53:43 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	while (alphabet <= 'z')
+		_putchar(alphabet);
+
+	_putchar('\n');
+
+	return (0);
+}
