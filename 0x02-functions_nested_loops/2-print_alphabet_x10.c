f7555bfbe8ec2fc9b37c8834d95142e7a3abfb50
Trust Gumise <trust@tolvatechafrica.com>
2022-10-18 18:17:34 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int counter = 1;
+	char alphabet = 'a';
+
+	while (counter <= 10)
+	{
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			_putchar++;
+		}
+		counter++;
+		_putchar('\n);
+	}
+}
