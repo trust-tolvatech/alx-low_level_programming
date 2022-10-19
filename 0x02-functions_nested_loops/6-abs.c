b2f98612a2f6ffe5c1088135939fd8df0d29fe47
Trust Gumise <trust@tolvatechafrica.com>
2022-10-19 07:21:53 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of integer
+ * @i: integer to be computed
+ * Return: the absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
