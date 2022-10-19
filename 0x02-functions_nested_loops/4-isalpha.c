f16a407f13ec7e6d3a31c6145193fbf10f7d3178
Trust Gumise <trust@tolvatechafrica.com>
2022-10-18 18:56:04 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is an alphabet
+ * @c: the character
+ * Return: 1 if character is aletter, lowercase or uppercase and 0 if not
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' || c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
