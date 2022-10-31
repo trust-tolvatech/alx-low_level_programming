d411a0f9ce8dfc84132193b23fb1b7a535d126a9
Trust Gumise <trust@tolvatechafrica.com>
2022-10-31 13:57:01 -0700

    Print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits number of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+	}
+
+	return (0);
+}
