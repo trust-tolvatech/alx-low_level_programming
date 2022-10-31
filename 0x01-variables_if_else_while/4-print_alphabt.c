8f8af6fda83d5b9a41e16b288a36f7da7a8b3fa7
Trust Gumise <trust@tolvatechafrica.com>
2022-10-31 13:52:25 -0700

    Print alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase without q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != 'q' && lc != e)
+			putchar(lc);
+	}
+	putchar('\n');
+
+	return (0);
+}
