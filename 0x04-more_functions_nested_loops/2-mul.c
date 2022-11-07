90980f2c41488f5f8e005b79f74888a84d7c0654
Trust Gumise <trust@tolvatechafrica.com>
2022-11-07 11:33:49 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
