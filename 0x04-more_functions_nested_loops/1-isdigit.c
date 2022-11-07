bfe1981e2d2b153eec60321291924e9ac9de1899
Trust Gumise <trust@tolvatechafrica.com>
2022-11-07 11:23:07 -0800

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - checks for a digit
+ * @c: the digit
+ *
+ * Return: 1 if c is digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
