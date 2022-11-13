48039e8df92d7e798c9833226c3e399bd09924c3
Trust Gumise <trust@tolvatechafrica.com>
2022-11-13 15:46:57 -0800

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 /**
  * print_numbers - prints numbers 1 to 9
  *
- * Return: void
+ * Return: Always 0
  */
 void print_numbers(void)
 {
-	char c;
+        char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+        for (c = '0'; c <= '9'; c++)
+        {
+                _putchar(c);
+        }
+        _putchar('\n');
+
+	return (0);
 }
