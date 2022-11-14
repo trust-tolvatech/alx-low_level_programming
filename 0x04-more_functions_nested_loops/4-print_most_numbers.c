9138108cd741c00f5b72e805769c2df5959ca2cc
Trust Gumise <trust@tolvatechafrica.com>
2022-11-13 16:29:12 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
