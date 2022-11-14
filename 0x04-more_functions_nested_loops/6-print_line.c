6ddec573de94d8fb8d81bfc374d219616a2d2561
Trust Gumise <trust@tolvatechafrica.com>
2022-11-13 17:35:51 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,14 @@ void print_line(int n)
 {
 	int x;
 
-	for (x = 0; x < n; x++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+		for (x = 0;; x < n; x++)
 		{
 			_putchar(95);
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
