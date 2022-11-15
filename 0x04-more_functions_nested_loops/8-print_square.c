8c747a97c929174ff154b982d3fc3e9370d10000
Trust Gumise <trust@tolvatechafrica.com>
2022-11-15 09:41:26 -0800

    Print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: integer params
+ */
+void print_square(int size)
+{
+	int x;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (x = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+		}
+		_putchar('\n');
+	}
+}
