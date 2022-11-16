2e2de4a0829b7713c57f10adfac9483f3ef3bf22
Trust Gumise <trust@tolvatechafrica.com>
2022-11-16 05:44:57 -0800

    hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,6 @@
 /**
  * fizz_buzz - printing to 100
  *
- * Return: Always(0)
  */
 void fizz_buzz(void)
 {
@@ -12,7 +11,9 @@ void fizz_buzz(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
+		if (n == 100)
+			printf("Buzz");
+		else if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
 		else if (n % 5 == 0)
 			printf("Buzz ");
