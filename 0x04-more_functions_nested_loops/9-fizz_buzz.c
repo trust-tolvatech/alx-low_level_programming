cb66267d3543f65e1c374a667117fc15e04e1f47
Trust Gumise <trust@tolvatechafrica.com>
2022-11-16 01:58:56 -0800

    hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * fizz_buzz - printing to 100
@@ -12,13 +13,13 @@ void fizz_buzz(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		else if (n % 5 == 0)
-			printf(" Buzz");
+			printf("Buzz ");
 		else if (n % 3 == 0)
-			printf(" Fizz");
+			printf("Fizz ");
 		else
-			printf(" %d", n);
+			printf("%d ", n);
 	}
 	printf("\n");
 }
