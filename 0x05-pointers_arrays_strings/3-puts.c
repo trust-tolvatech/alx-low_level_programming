96ed3de317b7d7f44d6a2fd93a8c670a0aedeabe
Trust Gumise <trust@tolvatechafrica.com>
2022-11-30 06:37:01 -0800

    Print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts - prints a string, followd by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
+}
